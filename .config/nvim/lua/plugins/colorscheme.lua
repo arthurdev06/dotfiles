026bc0987b5c1b5632eef6f8b634f15ef09a80cc
arthurdev06 <arthurdev6@gmail.com>
2024-07-19 23:12:28 -0300

    change config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  "kepano/flexoki-neovim",
-  name = "flexoki",
+  "elvessousa/sobrio",
+  name = "sobrio",
   lazy = false,
   priority = 1000,
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    vim.cmd.colorscheme("flexoki-dark")
+    vim.cmd.colorscheme("sobrio")
   end,
 }
